6010b006e681a3b2ea0bf185a91798885c8662bd
Brooke <Brooke.horrocks7@gmail.com>
2019-03-27 00:03:24 -0600

    lost project page changes, add basics back to project page

--- src/main/Projects.js
+++ src/main/Projects.js
@@ -4,6 +4,7 @@ import SpaIcon from "@material-ui/icons/Spa";
 
 import buffyAngelGif from "../assets/img/buffy-angel.gif";
 import bookItGif from "../assets/img/book-it.gif";
+import choreNinjaGif from "../assets/img/chore-ninja.gif";
 
 function Projects() {
     return (
@@ -16,7 +17,9 @@ function Projects() {
             </div>
             <section className="project-section">
                 <img className="project-gif" src={buffyAngelGif} alt=""/>
-                <h5>Project 1</h5>
+                <h3>Buffy-Angelverse</h3>
+                <h5>MVP: 10 days --- Status: Complete</h5>
+                <a className="project-link" target="_blank" href="http://buffy-angel.surge.sh" rel="noopener noreferrer">See it here</a>
             </section>
             <div className="hr-theme-slash-2">
                 <div className="hr-line"></div>
@@ -24,8 +27,10 @@ function Projects() {
                 <div className="hr-line"></div>
             </div>
             <section className="project-section">
-                Project 2
                 <img className="project-gif" src={bookItGif} alt=""/>
+                <h3>Book It</h3>
+                <h5>MVP: 10 days --- Status: Complete</h5>
+                <a className="project-link" target="_blank" href="http://bookitbooks.herokuapp.com/" rel="noopener noreferrer">See it here</a>
             </section>
             <div className="hr-theme-slash-2">
                 <div className="hr-line"></div>
@@ -33,7 +38,10 @@ function Projects() {
                 <div className="hr-line"></div>
             </div>
             <section className="project-section">
-                Project 3
+                <img className="project-gif" src={choreNinjaGif} alt=""/>
+                <h3>Chore Ninja</h3>
+                <h5>MVP: 10 days --- Status: In Progress</h5>
+                <a className="project-link" target="_blank" href="https://github.com/ashgallo/chore-ninja" rel="noopener noreferrer">See it on github here</a>
             </section>
         </div>
     )
